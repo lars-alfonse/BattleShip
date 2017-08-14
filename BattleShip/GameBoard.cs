4ec26542959bdf50cf893efc2b5c546ebf48b9d5
lars-alfonse <lars.alfonse@gmail.com>
2017-08-14 10:38:27 -0500

    Added Ship parent class, ship child classes, ship piece class and gameboard class

---
+++ BattleShip/GameBoard.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BattleShip
+{
+    abstract class GameBoard
+    {
+        string[,] gameBoard = new string[10, 10];
+
+    }
+}
